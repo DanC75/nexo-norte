7955ff386a90d4b4fb0c019267179c052a5f6c93
Dani <146557842+DanC75@users.noreply.github.com>
2026-07-15 09:25:32 -0500

    test: isolate static file middleware

--- config/settings/test.py
+++ config/settings/test.py
@@ -6,4 +6,9 @@ DEBUG = False
 SECRET_KEY = "test-only-secret-key"
 if not os.getenv("POSTGRES_HOST"):
     DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+MIDDLEWARE = [
+    middleware
+    for middleware in MIDDLEWARE  # noqa: F405
+    if middleware != "whitenoise.middleware.WhiteNoiseMiddleware"
+]
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
